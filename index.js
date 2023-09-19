422ecb85be3ecad3dfda2fff2fd86ca3c98ce228
LeonGitonga <leon.gitonga@student.moringaschool.com>
2023-09-19 21:52:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// code your solution here
+
+
+function saturdayFun(activity="roller-skate")  {
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+function mondayWork(work = "go to the office")  {
+    return `This Monday, I will ${work}.`;
+}
+
+function wrapAdjective(visualflair = "*"){
+    return function (adj = "special") {
+        return `You are ${visualflair}${adj}${visualflair}!`
+    }
+}
+const promptFunction = wrapAdjective("*");
+const compliment = promptFunction("awesome");
+console.log(compliment);
+
+
+
+const finalcall = wrapAdjective("%")("a dedicated programmer");
+console.log(finalcall);
